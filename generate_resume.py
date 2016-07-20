102c820ed5c8a8aedf34656a96c7a6ee5642c606
Chris Zacny <czacny@BELC02MM0SKFD58.sea.corp.expecn.com>
2016-07-20 12:42:21 -0700

    Fix read write bugs

--- generate_resume.py
+++ generate_resume.py
@@ -14,11 +14,11 @@ def main():
     print('Welcome to the Zacny resume generator.')
 
     my_working_directory = os.path.dirname(os.path.realpath(__file__))
-    defaults_file = os.path.join(my_working_directory, 'defaults.json')
+    defaults_file_path = os.path.join(my_working_directory, 'defaults.json')
 
-    if os.path.exists(defaults_file):
-        with open(defaults_file, 'r') as defaults_file:
-            resume_variable_data = json.loads(defaults_file.read())
+    if os.path.exists(defaults_file_path):
+        with open(defaults_file_path, 'r') as defaults_file_hdl:
+            resume_variable_data = json.loads(defaults_file_hdl.read())
     else:
         resume_variable_data = {Keys.Phone: '', Keys.EmailAddress: '', Keys.Website: '',
                                 Keys.Location: ''}
@@ -28,7 +28,14 @@ def main():
     website = input('Please enter your website [{}]: '.format(resume_variable_data[Keys.Website]))
     location = input('Please enter your location [{}]: '.format(resume_variable_data[Keys.Location]))
 
-    resume_variable_data = {Keys.Phone: phone, Keys.EmailAddress: email_address, Keys.Website: website, Keys.Location: location}
+    if email_address != '':
+        resume_variable_data[Keys.EmailAddress] = email_address
+    if phone != '':
+        resume_variable_data[Keys.Phone] = phone
+    if website != '':
+        resume_variable_data[Keys.Website] = website
+    if location != '':
+        resume_variable_data[Keys.Location] = location
 
     env = Environment(loader=PackageLoader('generate_resume', 'site'))
     template = env.get_template('template.html')
@@ -36,8 +43,11 @@ def main():
     with open(os.path.join(my_working_directory, 'site', 'resume.html'), 'w') as to_write:
         to_write.write(template.render(model=resume_variable_data))
 
-    with open(defaults_file, 'w') as defaults_writer:
+    with open(defaults_file_path, 'w') as defaults_writer:
         defaults_writer.write(json.dumps(resume_variable_data))
+
+    #TODO: Convert output to pdf, only first page.
+
     print('Done.')
 
 if __name__ == '__main__':
